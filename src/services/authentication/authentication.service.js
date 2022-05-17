2d461d573d09b69156e209d6897f7c2316f65b77
HectorLobatoSilva <lobato_man@hotmail.com>
2022-05-17 10:18:28 -0500

    [Fix]remove firebase functions

--- src/services/authentication/authentication.service.js
+++ src/services/authentication/authentication.service.js
@@ -1,7 +1,14 @@
 import firebase from 'firebase';
 import 'firebase/auth';
 
-var firebaseConfig = {};
+var firebaseConfig = {
+    apiKey: 'AIzaSyA7kRurl_E1C5NvtpwVzhWmyWVJ594adnc',
+    authDomain: 'mealstogo-60f0c.firebaseapp.com',
+    projectId: 'mealstogo-60f0c',
+    storageBucket: 'mealstogo-60f0c.appspot.com',
+    messagingSenderId: '469892448465',
+    appId: '1:469892448465:web:d65650310203838be305f0',
+};
 // Initialize Firebase
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
